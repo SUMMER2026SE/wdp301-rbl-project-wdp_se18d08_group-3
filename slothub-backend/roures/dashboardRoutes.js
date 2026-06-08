0123e7ec44a8e2bba15b1765927898931aa86824
quin2401 <huydqde170488@fpt.edu.vn>
2026-06-08 23:22:49 +0700

    Feat: implement dashboard analytics and fix routes folder typo

--- slothub-backend/roures/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAdminStats } = require('../controllers/dashboardController');
-const { protect, authorize } = require('../middlewares/authMiddleware');
-
-// Chốt chặn: Bắt buộc đăng nhập VÀ phải là 'Admin'
-router.get('/', protect, authorize('Admin'), getAdminStats);
-
-module.exports = router;
